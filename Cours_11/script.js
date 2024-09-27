1ec4b8395c8b3c988b65e3609c063e2ab4491e25
Victoria Nevzorova <2395016@bdeb.qc.ca>
2024-09-27 14:08:18 -0400

    TP et cours 11

---
+++ Cours_11/script.js
@@ -0,0 +1,8 @@
+let admin, nom, resultat, popup;
+nom = "Jean";
+admin = nom;
+alert(admin);
+resultat = prompt("titleText", ["defaultText"]);
+popup = confirm("heres a question");
+let estPlusGrand = 4 > 1;
+alert(estPlusGrand ); 
